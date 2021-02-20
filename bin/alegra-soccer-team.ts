64d5f09b93edf03357b377b74fa539c4cfe32e54
Leonardo Oliveros <leonardo.oliveros@alegra.com>
2021-02-20 15:30:55 -0500

    Initial commit

--- bin/alegra-soccer-team.ts
+++ bin/alegra-soccer-team.ts
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { AlegraSoccerTeamStack } from '../lib/alegra-soccer-team-stack';
+import * as dotenv from "dotenv";
+import { DynamoStack } from '../lib/dynamo-stack';
+
+dotenv.config();
 
 const app = new cdk.App();
-new AlegraSoccerTeamStack(app, 'AlegraSoccerTeamStack');
+const appName = "alegra-soccer-team";
+const env = process.env.ENVIRONMENT || "";
+
+if (["testing", "production"].indexOf(env) === -1) {
+  throw Error("Env not supported");
+}
+
+const sharedProps = {
+  env: env,
+  account: process.env.AWS_ACCOUNT_ID || "",
+  region: process.env.AWS_ACCOUNT_REGION || "",
+};
+
+new DynamoStack(app, 'DynamoStack', {
+  ...sharedProps,
+  name: `${appName}-dynamo-${env}`
+});
